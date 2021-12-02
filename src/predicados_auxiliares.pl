c44a70b6f156493bab3fae466826ccbbe24d1a6f
srcast <61480049+srcast@users.noreply.github.com>
2021-12-02 22:35:38 +0000

    Update predicados_auxiliares.pl

--- src/predicados_auxiliares.pl
+++ src/predicados_auxiliares.pl
@@ -129,7 +129,7 @@ calculaValor([(V, P, KM)|T], R) :-
         custoTransporte(V, P, KM, Custo),
 	calculaValor(T, Custo2),
 	R1 is Custo + Custo2,
-	round(R1, R, 1).
+	round(R1, R, 1), !.
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
