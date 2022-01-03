8d02571e1593ec9412c805f03119d8ef27611593
srcast <61480049+srcast@users.noreply.github.com>
2022-01-03 02:49:43 +0000

    Update base_conhecimento.pl

--- 2ª Fase/src/base_conhecimento.pl
+++ 2ª Fase/src/base_conhecimento.pl
@@ -16,42 +16,43 @@ objetivo(greenDistribution).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado move: LocalidadeOigem, LocalidadeDestino, CustoDistancia, CustoTempo -> {V,F}
-%move(fraiao,maximinos,2,3).
-%move(fraiao,celeiros,6,5).
+move(fraiao,maximinos,2,3).
+move(fraiao,celeiros,6,5).
 move(fraiao,palmeira,4,3).
 move(maximinos,celeiros,1,1).
 move(celeiros,gondizalves,1,3).
 move(palmeira,gondizalves,9,7).
 move(gondizalves,greenDistribution,2,9).
-%move(adaufe,mireDeTibaes,5,2).
+move(adaufe,mireDeTibaes,5,2).
 move(adaufe,gualtar,9,3).
-%move(adaufe,greenDistribution,5,5).
+move(adaufe,greenDistribution,5,5).
 move(mireDeTibaes,saoVitor,2,3).
 move(gualtar,saoVitor,6,4).
 move(gualtar,greenDistribution,1,9).
 move(saoVitor,semelhe,3,1).
 move(semelhe,trandeiras,8,7).
-%move(trandeiras,real,2,3).
+move(trandeiras,real,2,3).
 move(real,saoVicente,5,9).
 move(real,greenDistribution,8,2).
 move(saoVicente,pedralva,6,2).
-%move(saoVicente,tenoes,4,1).
+move(saoVicente,tenoes,4,1).
 move(pedralva,priscos,7,4).
 move(tenoes,cividade,2,8).
-%move(tenoes,priscos,1,1).
+move(tenoes,priscos,1,1).
 move(priscos,padimDaGraca,4,8).
 move(cividade,crespos,9,4).
 move(cividade,greenDistribution,3,5).
 move(padimDaGraca,crespos,2,4).
-%move(padimDaGraca,ferreiros,3,2).
+move(padimDaGraca,ferreiros,3,2).
 move(crespos,tadim,7,3).
-%move(ferreiros,tadim,5,5).
-%move(ferreiros,celeirosDup,1,9).
-%move(tadim,nogueira,3,7).
+move(ferreiros,tadim,5,5).
+move(ferreiros,celeirosDup,1,9).
+move(tadim,nogueira,3,7).
 move(tadim,greenDistribution,4,9).
-%move(celeirosDup,nogueira,1,3).
+move(celeirosDup,nogueira,1,3).
 
-%adjacente(Origem,Destino,D,T) :- adjacente(Destino,Origem,D,T).
+adjacente(Nodo, ProxNodo, C) :- move(Nodo, ProxNodo, C, _).
+adjacente(Nodo, ProxNodo, C) :- move(ProxNodo, Nodo, C, _).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -63,36 +64,39 @@ move(tadim,greenDistribution,4,9).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado entrega: estafeta, veiculo, distancia, tipoEncomenda, pesoEnc, prazo, velocidade, cliente, rua, classificacao, dataEncomenda, dataEntrega -> {V, F}
-entrega(manuel, bicicleta, 12, comida, 2, 1, 10, maria, 'crespos', 4, 11/10/2021/10, 11/10/2021/11).
-entrega(manuel, bicicleta, 12, comida, 2, 1, 10, maria, 'crespos', 4, 11/10/2021/13, 11/10/2021/14).
-entrega(manuel, bicicleta, 12, comida, 2, 1, 10, maria, 'crespos', 4, 11/10/2021/16, 11/10/2021/18).
-entrega(manuel, bicicleta, 12, comida, 2, 1, 10, maria, 'crespos', 4, 11/10/2021/12, 11/10/2021/13).
-entrega(jose, bicicleta, 15, comida, 4, 1, 10, ana, 'nogueira', 5, 11/10/2021/12, 11/10/2021/13).
-entrega(fabio, mota, 20, roupa, 12, 24, 35, filipa, 'tadim', 5, 12/10/2021/13, 12/10/2021/19).
-entrega(marco, carro, 23, movel, 80, 48, 25, cristina, 'adaufe', 3, 11/10/2021/17, 14/10/2021/10).
-entrega(jose, bicicleta, 15, comida, 4, 1, 10, ana, 'nogueira', 5, 11/10/2021/20, 11/10/2021/21).
-entrega(manuel, bicicleta, 15, comida, 1, 1, 10, ana, 'nogueira', 5, 11/10/2021/19, 11/10/2021/20).
-entrega(fabio, bicicleta, 15, comida, 2, 1, 10, ana, 'nogueira', 3, 12/10/2021/21, 12/10/2021/22).
-entrega(manuel, bicicleta, 15, comida, 1, 1, 10, ana, 'nogueira', 5, 11/10/2021/13, 11/10/2021/15).
-entrega(manuel, mota, 12, roupa, 10, 18, 35, maria, 'crespos', 3, 12/10/2021/11, 13/10/2021/10).
-entrega(manuel, mota, 23, comida, 3, 0.5, 35, cristina, 'adaufe', 4, 12/10/2021/14, 12/10/2021/15).
-entrega(manuel, carro, 20, movel, 74, 30, 25, filipa, 'tadim', 5, 13/10/2021/14, 16/10/2021/10).
-entrega(manuel, carro, 23, comida, 3, 0.5, 25, cristina, 'adaufe', 4, 15/10/2021/18, 15/10/2021/19).
-entrega(jose, carro, 15, roupa, 17, 4, 25, ana, 'nogueira', 2, 15/10/2021/18, 17/10/2021/10).
-entrega(jose, mota, 12, comida, 2, 1, 35, maria, 'crespos', 3, 14/10/2021/11, 14/10/2021/12).
-entrega(jose, mota, 20, comida, 3, 0.5, 35, filipa, 'tadim', 1, 12/10/2021/10, 12/10/2021/12).
-entrega(jose, carro, 20, movel, 74, 30, 25, filipa, 'tadim', 5, 13/10/2021/9, 16/10/2021/10).
-entrega(jose, carro, 15, movel, 60, 5, 25, ana, 'nogueira', 3, 14/10/2021/15, 15/10/2021/10).
-entrega(fabio, carro, 15, movel, 45, 17, 25, ana, 'nogueira', 2, 15/10/2021/15, 18/10/2021/10).
-entrega(fabio, mota, 12, roupa, 9, 5, 35, maria, 'crespos', 3, 12/10/2021/16, 14/10/2021/10).
-entrega(fabio, mota, 20, comida, 4, 2, 35, filipa, 'tadim', 1, 12/10/2021/21, 12/10/2021/22).
-entrega(fabio, carro, 23, movel, 53, 25, 25, cristina, 'adaufe', 2, 13/10/2021/4, 15/10/2021/10).
-entrega(fabio, bicicleta, 15, comida, 2, 1, 10, ana, 'nogueira', 3, 12/10/2021/13, 12/10/2021/14).
-entrega(marco, bicicleta, 15, roupa, 3, 1, 10, ana, 'nogueira', 2, 14/10/2021/9, 14/10/2021/18).
-entrega(marco, mota, 12, comida, 4, 0.5, 35, maria, 'crespos', 4, 12/10/2021/10, 12/10/2021/12).
-entrega(marco, mota, 23, comida, 3, 0.5, 35, cristina, 'adaufe', 5, 12/10/2021/11, 12/10/2021/12).
-entrega(marco, carro, 20, movel, 74, 30, 25, filipa, 'tadim', 5, 13/10/2021/11, 16/10/2021/10).
-entrega(marco, carro, 23, movel, 89, 23, 25, cristina, 'adaufe', 5, 15/10/2021/14, 19/10/2021/10).
+
+% retirei veiculo, distancia e velocidade
+
+entrega(manuel, comida, 2, 1, maria, 'crespos', 4, 11/10/2021/10, 11/10/2021/11).
+entrega(manuel, comida, 2, 1, maria, 'crespos', 4, 11/10/2021/13, 11/10/2021/14).
+entrega(manuel, comida, 2, 1, maria, 'crespos', 4, 11/10/2021/16, 11/10/2021/18).
+entrega(manuel, comida, 2, 1, maria, 'crespos', 4, 11/10/2021/12, 11/10/2021/13).
+entrega(jose, comida, 4, 1, ana, 'nogueira', 5, 11/10/2021/12, 11/10/2021/13).
+entrega(fabio, roupa, 12, 24, filipa, 'tadim', 5, 12/10/2021/13, 12/10/2021/19).
+entrega(marco, movel, 80, 48, cristina, 'adaufe', 3, 11/10/2021/17, 14/10/2021/10).
+entrega(jose, comida, 4, 1, ana, 'nogueira', 5, 11/10/2021/20, 11/10/2021/21).
+entrega(manuel, comida, 1, 1, ana, 'nogueira', 5, 11/10/2021/19, 11/10/2021/20).
+entrega(fabio, comida, 2, 1, ana, 'nogueira', 3, 12/10/2021/21, 12/10/2021/22).
+entrega(manuel, comida, 1, 1, ana, 'nogueira', 5, 11/10/2021/13, 11/10/2021/15).
+entrega(manuel, roupa, 10, 18, maria, 'crespos', 3, 12/10/2021/11, 13/10/2021/10).
+entrega(manuel, comida, 3, 0.5, cristina, 'adaufe', 4, 12/10/2021/14, 12/10/2021/15).
+entrega(manuel, movel, 74, 30, filipa, 'tadim', 5, 13/10/2021/14, 16/10/2021/10).
+entrega(manuel, comida, 3, 0.5, cristina, 'adaufe', 4, 15/10/2021/18, 15/10/2021/19).
+entrega(jose, roupa, 17, 4, ana, 'nogueira', 2, 15/10/2021/18, 17/10/2021/10).
+entrega(jose, comida, 2, 1, maria, 'crespos', 3, 14/10/2021/11, 14/10/2021/12).
+entrega(jose, comida, 3, 0.5, filipa, 'tadim', 1, 12/10/2021/10, 12/10/2021/12).
+entrega(jose, movel, 74, 30, filipa, 'tadim', 5, 13/10/2021/9, 16/10/2021/10).
+entrega(jose, movel, 60, 5, ana, 'nogueira', 3, 14/10/2021/15, 15/10/2021/10).
+entrega(fabio, movel, 45, 17, ana, 'nogueira', 2, 15/10/2021/15, 18/10/2021/10).
+entrega(fabio, roupa, 9, 5, maria, 'crespos', 3, 12/10/2021/16, 14/10/2021/10).
+entrega(fabio, comida, 4, 2, filipa, 'tadim', 1, 12/10/2021/21, 12/10/2021/22).
+entrega(fabio, movel, 53, 25, cristina, 'adaufe', 2, 13/10/2021/4, 15/10/2021/10).
+entrega(fabio, comida, 2, 1, ana, 'nogueira', 3, 12/10/2021/13, 12/10/2021/14).
+entrega(marco, roupa, 3, 1, ana, 'nogueira', 2, 14/10/2021/9, 14/10/2021/18).
+entrega(marco, comida, 4, 0.5, maria, 'crespos', 4, 12/10/2021/10, 12/10/2021/12).
+entrega(marco, comida, 3, 0.5, cristina, 'adaufe', 5, 12/10/2021/11, 12/10/2021/12).
+entrega(marco, movel, 74, 30, filipa, 'tadim', 5, 13/10/2021/11, 16/10/2021/10).
+entrega(marco, movel, 89, 23, cristina, 'adaufe', 5, 15/10/2021/14, 19/10/2021/10).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -144,7 +148,7 @@ desconto_velocidade(carro, Vel, Peso) :- Vel is Vel - (0.1*PesoEnc).
 
 %executar esta
 profundidade(NodoObjetivo, Caminho, C) :- inicial(Inicio),
-										profundidadeprimeiroInicial(NodoObjetivo, Inicio, [Inicio], Caminho, C), !.
+										profundidadeprimeiroInicial(NodoObjetivo, Inicio, [Inicio], Caminho, C).
 
 
 profundidadeprimeiroInicial(NodoObjetivo, Inicio, Historico, [Inicio, NodoObjetivo], C) :- adjacente(Inicio, NodoObjetivo, C).
@@ -164,13 +168,8 @@ profundidadeprimeiro(NodoObjetivo, NodoAtual, Historico, [ProxNodo|Caminho], C) 
 
 
 
-adjacente(Nodo, ProxNodo, C) :- move(Nodo, ProxNodo, C, _).
-adjacente(Nodo, ProxNodo, C) :- move(ProxNodo, Nodo, C, _).
 
-
-
-
-melhorProfundidade(NodoObjetivo, Caminho, Custo) :- findall((SS, CC), profundidade(NodoObjetivo, SS, CC), L), 
+melhorProfundidade(NodoObjetivo, Caminho, Custo) :- findall((Caminhos, Custos), profundidade(NodoObjetivo, Caminhos, Custos), L), 
 													minimo(L, (Caminho, Custo)), !. 
 
 
@@ -257,22 +256,93 @@ auxiliar([LA|Outros], Todos) :- LA = [Atual|_],
 
 calcula_circuitos_profundidade(Circuitos):- findall(
 (Estafeta,Veiculo,Distancia,Tempo,Caminho),
-(entrega(Estafeta, _ , _, _ , Peso, Prazo, _ , _ , NodoObjetivo, _ , _ , _ ),
+(entrega(Estafeta, _ , Peso, Prazo, _ , NodoObjetivo, _ , _ , _ ),
 melhorProfundidade(NodoObjetivo,Caminho,Distancia),
 escolheVeiculo(Peso,Distancia,Veiculo,Prazo,Tempo)),Circuitos).
 
 calcula_circuitos_largura(Circuitos):- findall(
 (Estafeta,Veiculo,Distancia,Tempo,Caminho),
-(entrega(Estafeta, _ , _, _ , Peso, Prazo, _ , _ , NodoObjetivo, _ , _ , _ ),
+(entrega(Estafeta, _ , Peso, Prazo, _ , NodoObjetivo, _ , _ , _ ),
 melhorLargura(NodoObjetivo,Caminho,Distancia),
 escolheVeiculo(Peso,Distancia,Veiculo,Prazo,Tempo)),Circuitos).
 
 
 % calcula_circuitos_profundidadeLimite(Circuitos):- findall(
 % (Estafeta,Veiculo,Distancia,Tempo,Caminho),
-% (entrega(Estafeta, _ , _, _ , Peso, Prazo, _ , _ , NodoObjetivo, _ , _ , _ ),
+% (entrega(Estafeta, _ , Peso, Prazo, _ , NodoObjetivo, _ , _ , _ ),
 % melhorProfundidadeLimite(NodoObjetivo,3,Caminho,Distancia),
-% scolheVeiculo(Peso,Distancia,Veiculo,Prazo,Tempo)),Circuitos).
+% escolheVeiculo(Peso,Distancia,Veiculo,Prazo,Tempo)),Circuitos).
+
+
+
+
+
+
+%--------------------------------------------------------------------------------------------------------------------------------
+% Gerar os circuitos de entrega, caso existam, que cubram um determinado território (e.g. rua ou freguesia);
+
+%gera um circuito para um determinado local
+geraCircuito(NodoObjetivo, Caminho, C) :- inicial(Inicio),
+										profundidadeprimeiroInicial(NodoObjetivo, Inicio, [Inicio], Caminho, C).
+
+% gera todos od circuitos para um determinado local
+geraCircuitosObjetivo(NodoObjetivo, Caminhos) :- findall(Caminho,geraCircuito(NodoObjetivo, Caminho, C), Caminhos).
+
+
+%--------------------------------------------------------------------------------------------------------------------------------
+% Identificar quais os circuitos com maior número de entregas (por volume e peso);
+
+
+%identificar circuitos com um minimo de Peso
+identificarCircuitos(Peso, Circuitos) :- geraCircuitosObjetivo(NodoObjetivo, Caminhos),
+										calcularPesosCircuitosTodos(Caminhos, CaminhosPesos),
+										identificarPorPeso(CaminhosPesos, Peso, Circuitos).
+
+calcularPesosCircuitosTodos([], []).
+calcularPesosCircuitosTodos([Circuito1|T], [CircuitoPeso|OutrosCircuitosPeso]) :- calculaPesoCircuito(Circuito1, CircuitoPeso),
+																				calcularPesosCircuitosTodos(T, OutrosCircuitosPeso).
+
+% retira a greenDistribution das contas
+calculaPesoCircuito([H|T], [H|T]/PesoTotal) :- calculaPesoCircuitoAuxiliar(T, PesoTotal).
+
+%calcula os pesos associados às freguesias do circuito
+calculaPesoCircuitoAuxiliar([], 0).
+calculaPesoCircuitoAuxiliar([Freguesia|T], PesoTotal) :- findall(Peso, entrega(_, _, Peso, _, _, Freguesia, _, _, _), ListaPesos),
+														somatorio(ListaPesos, Peso1),
+														calculaPesoCircuitoAuxiliar(T, Peso2),
+														PesoTotal is Peso1 + Peso2.
+
+%identifica os circuitos que tem pelo menos Peso
+identificarPorPeso([], Peso, []).
+identificarPorPeso([[H|T]/Peso1|Outros], Peso, [[H|T]/Peso1|Circuitos]) :- Peso1 >= Peso, !,
+																		identificarPorPeso(Outros, Peso, Circuitos).
+
+identificarPorPeso([[H|T]/Peso1|Outros], Peso, Circuitos) :- Peso1 < Peso, !,
+																		identificarPorPeso(Outros, Peso, Circuitos).
+
+
+
+%--------------------------------------------------------------------------------------------------------------------------------
+% Comparar circuitos de entrega tendo em conta os indicadores de produtividade;
+
+
+
+
+
+
+
+%--------------------------------------------------------------------------------------------------------------------------------
+% Escolher o circuito mais rápido (usando o critério da distância);
+
+
+
+
+
+
+%--------------------------------------------------------------------------------------------------------------------------------
+% Escolher o circuito mais ecológico (usando um critério de tempo);
+
+
 
 
 
@@ -302,3 +372,12 @@ membro(X, [_|Xs]):-
 
 escrever([]).
 escrever([X|L]):- write(X), nl, escrever(L).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado somatorio: Lista,Somatorio -> {V,F}
+% Calcula o somatório de uma lista
+somatorio([], 0).
+somatorio([H|T], R) :-
+        somatorio(T, R2),
+	R is H + R2.
